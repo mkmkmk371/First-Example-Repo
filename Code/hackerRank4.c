66ff9a8bd34bca182e94c4e736a5cc2fa3af25f7
mert <mertkalkan2000@icloud.com>
2023-07-22 17:56:13 +0300

    solved hackerrank 4 and 5

---
+++ Code/hackerRank4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a,int b,int c,int d){
+    a = a > b ? a : b;
+    c = c > d ? c : d;
+    a = a > c ? a : c;
+    return a;
+}
+
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
